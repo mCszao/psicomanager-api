49042ef73b98315b855750c6009af0fc12677410
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-05 05:16:36 -0400

    feat: new exception handler to control when schedule its been created

--- src/main/java/com/psicomanager/api/exceptions/patient/PatientNotFoundException.java
+++ src/main/java/com/psicomanager/api/exceptions/patient/PatientNotFoundException.java
@@ -2,8 +2,8 @@ package com.psicomanager.api.exceptions.patient;
 
 import com.psicomanager.api.exceptions.CustomException;
 
-public class PatientNotFound extends CustomException {
-    public PatientNotFound(String message) {
+public class PatientNotFoundException extends CustomException {
+    public PatientNotFoundException(String message) {
         super(message);
     }
 }
--- src/main/java/com/psicomanager/api/infra/config/ControllerExceptionHandler.java
+++ src/main/java/com/psicomanager/api/infra/config/ControllerExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.psicomanager.api.infra.config;
 
 import com.psicomanager.api.dtos.BaseResponse;
-import com.psicomanager.api.exceptions.user.DuplicateUserEntryException;
+import com.psicomanager.api.exceptions.DuplicateEntryException;
+import com.psicomanager.api.exceptions.patient.PatientNotFoundException;
+import com.psicomanager.api.exceptions.schedule.ScheduleConflictTimeException;
 import com.psicomanager.api.exceptions.user.UserNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +12,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     private ResponseEntity<BaseResponse> userNotFoundHandler(UserNotFoundException ex){
-        return ResponseEntity.badRequest().body(new BaseResponse(false, ex.getMessage()));
+        return ResponseEntity.badRequest().body(new BaseResponse<>(false, ex.getMessage()));
     }
 
-    @ExceptionHandler(DuplicateUserEntryException.class)
-    private ResponseEntity<BaseResponse> duplicateUserEntryHandler(DuplicateUserEntryException ex){
-        return ResponseEntity.badRequest().body(new BaseResponse(false, ex.getMessage()));
+    @ExceptionHandler(DuplicateEntryException.class)
+    private ResponseEntity<BaseResponse> duplicateUserEntryHandler(DuplicateEntryException ex){
+        return ResponseEntity.badRequest().body(new BaseResponse<>(false, ex.getMessage()));
+    }
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    private ResponseEntity<BaseResponse> patientNotFoundHandler(PatientNotFoundException ex){
+        return ResponseEntity.badRequest().body(new BaseResponse<>(false, ex.getMessage()));
+    }
+
+    @ExceptionHandler(ScheduleConflictTimeException.class)
+    private ResponseEntity<BaseResponse> scheduleConflictHandler(ScheduleConflictTimeException ex){
+        return ResponseEntity.badRequest().body(new BaseResponse<>(false, ex.getMessage()));
     }
 }
