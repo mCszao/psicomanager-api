5373394d6a83b4b26cb3bee331a346014d3b2c92
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-05 05:17:11 -0400

    refact: changing to base response

--- src/main/java/com/psicomanager/api/controllers/ScheduleController.java
+++ src/main/java/com/psicomanager/api/controllers/ScheduleController.java
@@ -1,6 +1,7 @@
 package com.psicomanager.api.controllers;
 
 import com.psicomanager.api.domain.schedule.ScheduleRegisterDTO;
+import com.psicomanager.api.dtos.BaseResponse;
 import com.psicomanager.api.services.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,8 @@ public class ScheduleController {
     @Autowired
     private ScheduleService service;
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody ScheduleRegisterDTO data){
+    public ResponseEntity<BaseResponse> register(@RequestBody ScheduleRegisterDTO data){
         service.createSchedule(data);
-        return ResponseEntity.ok("Agendamento realizado com sucesso!");
+        return ResponseEntity.ok(new BaseResponse(true,"Agendamento realizado com sucesso!"));
     }
 }
--- src/main/java/com/psicomanager/api/services/ScheduleService.java
+++ src/main/java/com/psicomanager/api/services/ScheduleService.java
@@ -1,18 +1,13 @@
 package com.psicomanager.api.services;
 
-import com.psicomanager.api.domain.patient.Patient;
 import com.psicomanager.api.domain.schedule.Schedule;
 import com.psicomanager.api.domain.schedule.ScheduleRegisterDTO;
-import com.psicomanager.api.domain.schedule.StageEnum;
-import com.psicomanager.api.exceptions.patient.PatientNotFound;
+import com.psicomanager.api.exceptions.patient.PatientNotFoundException;
 import com.psicomanager.api.exceptions.schedule.ScheduleConflictTimeException;
 import com.psicomanager.api.repositories.PatientRepository;
 import com.psicomanager.api.repositories.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ScheduleService {
@@ -25,7 +20,7 @@ public class ScheduleService {
     public void createSchedule(ScheduleRegisterDTO dto){
         var schedules = scheduleRepo.getScheduleBetweenStartEnd(dto.dateStart(), dto.dateStart().plusHours(1));
         if(schedules.isEmpty()) {
-            var patient =  patientRepo.findById(dto.patientId()).orElseThrow(() -> new PatientNotFound("Paciente informado não encontrado"));
+            var patient =  patientRepo.findById(dto.patientId()).orElseThrow(() -> new PatientNotFoundException("Paciente informado não encontrado"));
             Schedule formedSchedule = new Schedule(dto, patient);
             scheduleRepo.save(formedSchedule);
             return;
