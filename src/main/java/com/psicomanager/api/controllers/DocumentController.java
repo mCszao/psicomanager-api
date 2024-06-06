171853dbed562b049448771a3133ab1153dd9de7
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-06 19:56:26 -0400

    refact: rewrite controller to return pdf

--- src/main/java/com/psicomanager/api/controllers/DocumentController.java
+++ src/main/java/com/psicomanager/api/controllers/DocumentController.java
@@ -3,6 +3,10 @@ import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import com.psicomanager.api.services.DocumentService;
 import org.apache.pdfbox.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,15 +23,14 @@ public class DocumentController {
     @Autowired
     private DocumentService service;
     @GetMapping("/generate-contract")
-    public void getContract(@RequestParam String patientId, HttpServletResponse response) throws IOException {
+    public ResponseEntity<byte[]> getContract(@RequestParam String patientId) throws IOException {
         byte[] pdfContract = service.generateContract(patientId);
-        response.setContentType("application/pdf");
-        response.setHeader("Content-Disposition", "attachment; filename=generated-contract.pdf");
-        response.setContentLength(pdfContract.length);
 
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(pdfContract);
-        IOUtils.copy(inputStream, response.getOutputStream());
-        response.flushBuffer();
-        response.setStatus(200);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=generated-contract.pdf");
+        headers.setContentLength(pdfContract.length);
+
+        return ResponseEntity.ok().headers(headers).body(pdfContract);
     }
 }
