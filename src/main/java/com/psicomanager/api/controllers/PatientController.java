23e170a003bf5188ffda3c40b1fdc6d7a26a74ad
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-05 22:34:17 -0400

    refact: rename routes to plural

--- src/main/java/com/psicomanager/api/controllers/PatientController.java
+++ src/main/java/com/psicomanager/api/controllers/PatientController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
-@RequestMapping("/patient")
+@RequestMapping("/patients")
 public class PatientController {
 
     @Autowired
