56a370517c0d4fe627d0b937dbb202714fea485d
Marcos Vinicius <marc.khk11@gmail.com>
2024-10-15 15:57:05 -0400

    fix: cannot add same zipcode

--- src/main/java/com/psicomanager/api/services/AddressPatientService.java
+++ src/main/java/com/psicomanager/api/services/AddressPatientService.java
@@ -2,6 +2,7 @@ package com.psicomanager.api.services;
 
 import com.psicomanager.api.domain.address.dto.AddressOnPatientDTO;
 import com.psicomanager.api.domain.address.model.Address;
+import com.psicomanager.api.domain.patient.exception.DuplicatePatientEntryException;
 import com.psicomanager.api.domain.patient.exception.PatientNotFoundException;
 import com.psicomanager.api.repositories.AddressRepository;
 import com.psicomanager.api.repositories.PatientRepository;
@@ -24,6 +25,11 @@ public class AddressPatientService {
     public void saveAddressPatient(AddressOnPatientDTO dto, String patientId){
         log.info("Buscando informações do paciente de id "+ patientId);
         var patient = patientRepo.findById(patientId).orElseThrow(() -> new PatientNotFoundException("Id do paciente informado não possui registro"));
+        if(!patient.getAddresses().isEmpty()) {
+            patient.getAddresses().forEach(address -> {
+                if(address.getZipcode().equals(dto.zipcode())) throw new DuplicatePatientEntryException("ZipCode/CEP já cadastrado para esse paciente");
+            });
+        }
         var address = new Address(dto, patient);
         log.info("Salvando endereço no paciente de id "+ patientId);
         addressRepo.save(address);
